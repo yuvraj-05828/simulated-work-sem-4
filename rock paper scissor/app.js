56595f7df991c704c8d6d8fb330d7a0e9ca5b6bd
yuvraj-05828 <yuvraj05828@gmail.com>
2025-01-16 16:00:37 +0530

    rock paper scissor

---
+++ rock paper scissor/app.js
@@ -0,0 +1,43 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
+
+possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
+    getResult()
+}))
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    if(randomNumber === 1){
+        computerChoice = 'rock'
+    }
+    if(randomNumber === 2){
+        computerChoice = 'paper'
+    }
+    if(randomNumber === 3){
+        computerChoice = 'scissors'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult(){
+    if (userChoice === computerChoice) {
+        result = 'Its a tie!'
+    } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') ||
+        (userChoice === 'paper' && computerChoice === 'rock')
+    ) {
+        result = 'You win!'
+    } else {
+        result = 'You lose!'
+    }
+    resultDisplay.innerHTML = result
+}
